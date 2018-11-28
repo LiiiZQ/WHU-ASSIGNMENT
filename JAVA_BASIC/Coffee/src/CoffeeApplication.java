71e7ceb51cdded810bf68043e241f582b952bfd8
Ziqing Li <45389890+LiiiiiZQ@users.noreply.github.com>
2018-11-28 16:24:45 +0800

    Add files via upload

---
+++ JAVA_BASIC/Coffee/src/CoffeeApplication.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+public class CoffeeApplication {
+	public static void Main(String[] arg) throws IOException {
+		
+		
+	}
+	//Display the catalog: lists the code and description of each product
+	//Display a product
+	//Display the current order: lists quantity, code, and price of each product in the current order, and the total of the order.
+	//Add a product to the current order—if the specified product is
+	//already part of the order, this command will modify the quantity of that product
+	//Remove a product from the current order
+	//Register the sale of the current order—this command adds the order to the store's sales and empties the current order
+	//Display the sales: lists all the orders that have been sold
+	
+	
+	public int getValue(){
+		return 1;
+	}
+	
+	
+	
+	
+
+}
